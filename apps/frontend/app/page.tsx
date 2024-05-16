6041fb3b97f5406e2d5329a3cdbd1b412a6f05f8
santigp258 <santigp258@gmail.com>
2024-05-16 17:54:07 -0500

    chore(ui): use Suspense

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,9 +1,12 @@
 import NextDatatableExample from './next-datatable-example';
+import { Suspense } from 'react';
 
 export default async function Index() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>
-      <NextDatatableExample />
+    <div className="h-screen w-screen flex justify-center items-center">
+      <Suspense fallback="loading...">
+        <NextDatatableExample />
+      </Suspense>
     </div>
   );
 }
