73bd05d48cdbb51f7c7080c863609d2420345d0d
santigp258 <santigp258@gmail.com>
2024-06-06 11:54:45 -0500

    chore(ui): improve sidebar implementation

--- packages/ui/src/components/composites/sidebar/sidebar.tsx
+++ packages/ui/src/components/composites/sidebar/sidebar.tsx
@@ -1,22 +1,26 @@
 import React, {
   FC,
   ForwardRefExoticComponent,
-  Fragment,
   HTMLAttributes,
+  memo,
   ReactNode,
+  useCallback,
   useImperativeHandle,
-  useMemo
+  useMemo,
 } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { Cross1Icon, HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
-  AccordionTrigger
-} from '@lifespikes/ui/components';
-import { cn, getUniqueId } from '@lifespikes/ui/lib/utils';
-import { useDisclose, UseDiscloseReturn } from '@lifespikes/ui/hooks';
+  AccordionTrigger,
+  cn,
+  getUniqueId,
+  Sheet,
+  SheetContent,
+  useDisclose,
+  UseDiscloseReturn,
+} from '@lifespikes/ui';
 
 export interface SidebarItem {
   id?: string;
@@ -46,15 +50,16 @@ export interface SidebarListProps {
   className?: string;
 }
 
-const SidebarListItem: FC<SidebarItem> = (item) => {
+const SidebarListItem: FC<{ item: SidebarItem }> = ({ item }) => {
   const hasChildren = (item.children ?? []).length > 0;
 
   const areSomeChildrenCurrent = (item.children ?? []).some(
     (item) => item.current
   );
 
-  const LinkWrapper = (
-    hasChildren ? AccordionTrigger : item.as ?? 'a'
+  const LinkWrapper = useMemo(
+    () => (hasChildren ? AccordionTrigger : item.as ?? 'a'),
+    [hasChildren, item.as]
   ) as FC<any>;
 
   return (
@@ -98,7 +103,7 @@ const SidebarListItem: FC<SidebarItem> = (item) => {
               className="-mx-0 mt-1"
               items={(item.children ?? []).map((child) => ({
                 ...child,
-                icon: () => <div className="w-10" />
+                icon: () => <div className="w-10" />,
               }))}
             />
           </AccordionContent>
@@ -108,154 +113,105 @@ const SidebarListItem: FC<SidebarItem> = (item) => {
   );
 };
 
-const SidebarList: FC<SidebarListProps> = ({ items, className }) => {
+const SidebarList: FC<SidebarListProps> = memo(({ items, className }) => {
   return (
     <ul className={cn('-mx-2 space-y-1', className)}>
       {items.map((item, idx) => (
         <li key={item.id ?? idx}>
-          <SidebarListItem {...item} />
+          <SidebarListItem item={item} />
         </li>
       ))}
     </ul>
   );
-};
+});
 
-export const Sidebar = React.forwardRef<SidebarRefType, SidebarProps>(
-  (
-    {
-      items = [],
-      renderLogo,
-      logo,
-      renderMenuButton,
-      hasMenuButton = true,
-      modal: modalProp
-    },
-    ref
-  ) => {
-    const modalDisclose = useDisclose();
-    const modal = modalProp ?? modalDisclose;
-    const { isOpen, onClose, onOpen } = modal;
+SidebarList.displayName = 'SidebarList';
 
-    useImperativeHandle(ref, () => modal, [modal]);
+export const Sidebar = memo(
+  React.forwardRef<SidebarRefType, SidebarProps>(
+    (
+      {
+        items = [],
+        renderLogo,
+        logo,
+        renderMenuButton,
+        hasMenuButton = true,
+        modal: modalProp,
+      },
+      ref
+    ) => {
+      const modalDisclose = useDisclose();
+      const modal = modalProp ?? modalDisclose;
+      const { isOpen, onOpen, setIsOpen } = modal;
 
-    const mappedItems = useMemo(
-      () =>
-        items.map((item) => ({ ...item, id: item.id || getUniqueId(items) })),
-      [items]
-    );
+      useImperativeHandle(ref, () => modal, [modal]);
 
-    const bottomItem = useMemo(
-      () => mappedItems.find((item) => item.toBottomPosition),
-      [mappedItems]
-    );
+      const mappedItems = useMemo(
+        () =>
+          items.map((item) => ({ ...item, id: item.id || getUniqueId(items) })),
+        [items]
+      );
 
-    const filteredItems = useMemo(
-      () => mappedItems.filter((item) => item.id !== bottomItem?.id),
-      [mappedItems, bottomItem]
-    );
+      const bottomItem = useMemo(
+        () => mappedItems.find((item) => item.toBottomPosition),
+        [mappedItems]
+      );
 
-    const navigationContent = (
-      <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-background px-6 pb-4">
-        <div className="flex h-16 shrink-0 items-center">
-          {renderLogo ? (
-            renderLogo()
-          ) : (
-            <img className="h-8 w-auto" src={logo} alt="Your Company" />
-          )}
-        </div>
-        <nav className="flex flex-1 flex-col">
-          <ul className="flex flex-1 flex-col gap-y-7 mt-4">
-            <li>
-              <SidebarList items={filteredItems} />
-            </li>
-            {bottomItem ? (
-              <li className="mt-auto">
-                <SidebarListItem {...bottomItem} />
-              </li>
-            ) : null}
-          </ul>
-        </nav>
-      </div>
-    );
+      const filteredItems = useMemo(
+        () => mappedItems.filter((item) => item.id !== bottomItem?.id),
+        [mappedItems, bottomItem]
+      );
 
-    return (
-      <div>
-        <Transition.Root show={isOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="relative z-50 lg:hidden"
-            onClose={() => onClose?.()}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-gray-900/80" />
-            </Transition.Child>
+      const handleOnOpen = useCallback(() => {
+        onOpen();
+      }, [onOpen]);
 
-            <div className="fixed inset-0 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                      <button
-                        type="button"
-                        className="-m-2.5 p-2.5"
-                        onClick={onClose}
-                      >
-                        <span className="sr-only">Close sidebar</span>
-                        <Cross1Icon
-                          className="h-6 w-6 text-white"
-                          aria-hidden="true"
-                        />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  {navigationContent}
-                </Dialog.Panel>
-              </Transition.Child>
+      return (
+        <div>
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetContent side="left" className="px-0"></SheetContent>
+          </Sheet>
+
+          <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-background px-6 pb-4 h-full">
+              <div className="flex h-16 shrink-0 items-center">
+                {renderLogo ? (
+                  renderLogo()
+                ) : (
+                  <img className="h-8 w-auto" src={logo} alt="Your Company" />
+                )}
+              </div>
+              <nav className="flex flex-1 flex-col">
+                <ul className="flex flex-1 flex-col gap-y-7 mt-4">
+                  <li>
+                    <SidebarList items={filteredItems} />
+                  </li>
+                  {bottomItem ? (
+                    <li className="mt-auto">
+                      <SidebarListItem item={bottomItem} />
+                    </li>
+                  ) : null}
+                </ul>
+              </nav>
             </div>
-          </Dialog>
-        </Transition.Root>
+          </div>
 
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          {navigationContent}
+          {renderMenuButton ? (
+            renderMenuButton(modal)
+          ) : hasMenuButton ? (
+            <button
+              type="button"
+              className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+              onClick={handleOnOpen}
+            >
+              <span className="sr-only">Open sidebar</span>
+              <HamburgerMenuIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          ) : null}
         </div>
-
-        {renderMenuButton ? (
-          renderMenuButton(modal)
-        ) : hasMenuButton ? (
-          <button
-            type="button"
-            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-            onClick={onOpen}
-          >
-            <span className="sr-only">Open sidebar</span>
-            <HamburgerMenuIcon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        ) : null}
-      </div>
-    );
-  }
+      );
+    }
+  )
 );
+
+Sidebar.displayName = 'Sidebar';
