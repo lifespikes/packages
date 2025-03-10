9aa5ecd37d9588dc4c4e9246a603cf1efbaf7f83
Luis Javier Moy <luisjavier1781@gmail.com>
2025-03-10 15:49:39 -0400

    feat: possibility to hide chevron down in accordion component

--- packages/ui/src/components/ui/accordion/accordion.tsx
+++ packages/ui/src/components/ui/accordion/accordion.tsx
@@ -20,8 +20,8 @@ AccordionItem.displayName = 'AccordionItem';
 
 const AccordionTrigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & {hasChevronDown?: boolean}
+>(({ className, children, hasChevronDown=true, ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       ref={ref}
@@ -32,7 +32,7 @@ const AccordionTrigger = React.forwardRef<
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      {hasChevronDown && <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
